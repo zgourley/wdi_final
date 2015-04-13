dcf4a39e1dde37c659c5f181878c7653974e2f3e
Glenn Harris <harris.glenn.jr@gmail.com>
2015-04-12 21:23:45 -0700

    add starter code for routes lesson

---
+++ 05-week/routes_lesson_starter_code/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  #You can have the root of your site routed with "root"
+
+  #GET request to /posts. Shows index of all posts
+  
+  #POST requests to /posts. Creates a new post
+
+  #GET request to '/posts/new'. Shows the form to create a new post
+
+  #GET request to '/posts/:id/edit'. Shows the edit form
+  
+  #GET request to '/posts/:id', shows individual post
+
+  #PUT & PATCH requests to '/posts/:id' will update an individual post
+
+  #DELETE request to '/posts/:id' destroys an individual post
+end
